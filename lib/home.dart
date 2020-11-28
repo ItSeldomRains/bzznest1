63a74d529f7517b44c26a9e1e762ec7d30bb4afd
ItSeldomRains <agmatthewarapista@gmail.com>
2020-11-29 00:18:03 +0800

    Improved market (home.dart)

--- lib/home.dart
+++ lib/home.dart
@@ -153,111 +153,147 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Column(
-        children: [
-          SizedBox(height: 30),
-          Container(
-            height: 70,
-            color: Colors.black,
-            child: Row(
-              children: [
-                SizedBox(width: 20),
-                isDrawerOpen
-                    ? IconButton(
-                        icon: Icon(Icons.arrow_back_ios),
-                        color: Colors.white,
+    return Stack(
+      children: [
+        Column(children: [
+          Column(
+            children: [
+              SizedBox(height: 10),
+              Container(
+                height: 70,
+                color: Colors.black,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    isDrawerOpen
+                        ? IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            color: Colors.white,
+                            onPressed: () {
+                              setState(() {
+                                xOffset = 0;
+                                isDrawerOpen = false;
+                              });
+                            })
+                        : IconButton(
+                            icon: Icon(Icons.menu, color: Colors.white),
+                            onPressed: () {
+                              setState(() {
+                                xOffset = 150;
+                                isDrawerOpen = true;
+                              });
+                            }),
+                    Container(
+                        child: Image.asset("assets/header.png"),
+                        height: 100,
+                        width: 300)
+                  ],
+                ),
+              )
+            ],
+          ),
+          AnimatedContainer(
+            height: 763,
+            width: 599,
+            transform: Matrix4.translationValues(xOffset, yOffset, 0)
+              ..scale(scaleFactor),
+            duration: Duration(milliseconds: 250),
+            color: Color(0xFF2e2a00),
+            child: ListView.builder(
+                itemCount: contentEach.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                        width: 100,
+                        height: 230,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(color: Colors.black, spreadRadius: 3),
+                          ],
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            stops: [0.1, 0.29, 0.3, 1],
+                            colors: [
+                              Colors.black12,
+                              Colors.black12,
+                              Colors.white,
+                              Colors.white
+                            ],
+                          ),
+                        ),
+                        child: Column(children: [
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              SizedBox(width: 10),
+                              Image.asset(contentEach[index]["ppic"],
+                                  height: 40, width: 40),
+                              SizedBox(width: 10),
+                              Text(contentEach[index]["bname"],
+                                  style: TextStyle(color: Colors.white)),
+                              SizedBox(width: 10),
+                              Text(contentEach[index]["location"],
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10))
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Image.asset(contentEach[index]["picture"],
+                              width: 200, height: 100),
+                          SizedBox(height: 15),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.phone),
+                                Text("Contact"),
+                                SizedBox(width: 170),
+                                Icon(Icons.textsms_outlined),
+                                Text(contentEach[index]["comments"]),
+                                SizedBox(width: 10),
+                                Icon(Icons.thumb_up_alt),
+                                SizedBox(width: 5),
+                                Text(contentEach[index]["likes"])
+                              ])
+                        ])),
+                  );
+                }),
+          ),
+        ]),
+        Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Color(0xFF242424),
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(40))),
+                height: 200,
+                width: 60,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    RaisedButton(
+                        color: Color(0xFF242424),
+                        onPressed: () {},
+                        child: Icon(Icons.store, color: Colors.yellow)),
+                    RaisedButton(
+                        color: Color(0xFF242424),
                         onPressed: () {
-                          setState(() {
-                            xOffset = 0;
-                            isDrawerOpen = false;
-                          });
-                        })
-                    : IconButton(
-                        icon: Icon(Icons.menu, color: Colors.white),
+                          debugPrint("awerq");
+                        },
+                        child: Icon(Icons.favorite, color: Colors.white)),
+                    RaisedButton(
+                        color: Color(0xFF242424),
                         onPressed: () {
-                          setState(() {
-                            xOffset = 150;
-                            isDrawerOpen = true;
-                          });
-                        }),
-                Container(
-                    child: Image.asset("assets/header.png"),
-                    height: 100,
-                    width: 300)
-              ],
-            ),
-          )
-        ],
-      ),
-      AnimatedContainer(
-        height: 743,
-        width: 599,
-        transform: Matrix4.translationValues(xOffset, yOffset, 0)
-          ..scale(scaleFactor),
-        duration: Duration(milliseconds: 250),
-        color: Color(0xFF2e2a00),
-        child: ListView.builder(
-            itemCount: contentEach.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                    width: 100,
-                    height: 230,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(color: Colors.black, spreadRadius: 3),
-                      ],
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [0.1, 0.29, 0.3, 1],
-                        colors: [
-                          Colors.black45,
-                          Colors.black45,
-                          Colors.white,
-                          Colors.white
-                        ],
-                      ),
-                    ),
-                    child: Column(children: [
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          SizedBox(width: 10),
-                          Image.asset(contentEach[index]["ppic"],
-                              height: 40, width: 40),
-                          SizedBox(width: 10),
-                          Text(contentEach[index]["bname"],
-                              style: TextStyle(color: Colors.white)),
-                          SizedBox(width: 10),
-                          Text(contentEach[index]["location"],
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 10))
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Image.asset(contentEach[index]["picture"],
-                          width: 200, height: 100),
-                      SizedBox(height: 15),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.phone),
-                            Text("Contact"),
-                            SizedBox(width: 170),
-                            Icon(Icons.textsms_outlined),
-                            Text(contentEach[index]["comments"]),
-                            SizedBox(width: 10),
-                            Icon(Icons.thumb_up_alt),
-                            SizedBox(width: 5),
-                            Text(contentEach[index]["likes"])
-                          ])
-                    ])),
-              );
-            }),
-      )
-    ]);
+                          debugPrint("awerq");
+                        },
+                        child: Icon(Icons.drafts, color: Colors.white)),
+                  ],
+                )))
+      ],
+    );
   }
 }
